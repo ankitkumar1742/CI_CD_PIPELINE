d67c9cfa27d99ad517db2ef0f0b795b81b00c2d8
ankit kumar <a.kumar@channelassist.com>
2023-07-15 11:07:46 +0530

    changed background color

--- components/StarRating.tsx
+++ components/StarRating.tsx
@@ -53,6 +53,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'grey',
   },
   container: {
     flexDirection: 'row',
