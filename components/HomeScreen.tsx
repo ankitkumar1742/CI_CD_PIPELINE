c5c395e93e2e1cfb53d60926f30470511af460e9
ankit kumar <a.kumar@channelassist.com>
2023-07-15 11:04:46 +0530

    start rating functionality

--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -1,19 +1,21 @@
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React from 'react';
 import {useCustomContext} from './UserContext';
+import StarRating from './StarRating';
 
-const HomeScreen: React.FC = (props:any) => {
+const HomeScreen: React.FC = (props: any) => {
   const {userData, setUserData} = useCustomContext();
 
   const {firstName, lastName, isLoggedIn} = userData;
 
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      {isLoggedIn? (
+      {isLoggedIn ? (
         <Text>
           {firstName && firstName} {lastName && lastName}
         </Text>
-      ):null}
+      ) : null}
+      <StarRating />
       <TouchableOpacity
         onPress={() => props.navigation.navigate('Quiz')}
         // onPress={() => {
